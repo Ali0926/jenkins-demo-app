39f5b61d5b78950151e638ffc344ddf1ba75e93c
Ali <ali.saadi0926@gmail.com>
2025-12-23 19:28:56 -0700

    Update message

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello from Jenkins CI/CD!'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+@app.route("/")
+def hello():
+    return "Hello from Jenkins CI/CD!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+# this is is a simple of Flask application Flask that runs on when8080 and returns a simple message when accessed.

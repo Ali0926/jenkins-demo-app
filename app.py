5e5fd7bc3663e4fe81a5997a3fd0d09b571d6bff
Ali <ali.saadi0926@gmail.com>
2025-12-23 20:27:26 -0700

    updated message

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Jenkins CI/CD!"
+    return "Hello from Jenkins CI/CD! This is a sample application for Jenkins CI/CD pipeline."
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
-#Comment:
+# Comment:
 # this is is a simple of Flask application Flask that runs on when8080 and returns a simple message when accessed.

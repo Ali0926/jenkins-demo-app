4b558ddd5d23823b8c176552fd2520c443073bfa
Ali <ali.saadi0926@gmail.com>
2025-12-23 19:32:01 -0700

    updated comment

--- app.py
+++ app.py
@@ -10,4 +10,5 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+#Comment:
 # this is is a simple of Flask application Flask that runs on when8080 and returns a simple message when accessed.
